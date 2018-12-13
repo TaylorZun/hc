029a10315db2c61b968162ec95ed48397c835a4b
Taylorzun <33285361+TaylorZun@users.noreply.github.com>
2018-12-13 16:12:49 +0800

    Add files via upload

--- src/api/controller/index.js
+++ src/api/controller/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: pxf
+ * @Date: 2018-12-01 15:49:00
+ * @LastEditTime: 2018-12-12 22:52:49
+ * @LastEditors: your name
+ */
+
 const Base = require('./base.js')
 
 module.exports = class extends Base {
--- src/api/controller/sugar.js
+++ src/api/controller/sugar.js
@@ -1,3 +1,11 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: pxf
+ * @Date: 2018-12-08 17:03:19
+ * @LastEditTime: 2018-12-13 15:51:43
+ * @LastEditors: Please set LastEditors
+ */
+
 const Base = require('./base.js')
 
 module.exports = class extends Base {
@@ -5,19 +13,22 @@ module.exports = class extends Base {
      * sssss
      */
     async sugarhandAction() {
+        let data = this.post()  ///获取到提交上来的所有数据
+        console.log(data)
         const sugardata = this.post(data.sugardata)
         const timedian = this.post(data.timedian)
         const beizhu = this.post(data.beizhu)
-        const buffer = Buffer.from(beizhu)
-        const insertId = await this.model('sugarrecord').add({
+        // const buffer = Buffer.from(beizhu)
+        const insertId = await this.model('sugardata').add({
             sugardata: sugardata,
             timedian: timedian,
-            beizhu: buffer.toString('base64'),
+            // beizhu: buffer.toString('base64'),
+            beizhu: beizhu,
             add_time: this.getTime(),
-            user_id: this.getLoginUserId
+            //user_id: this.getLoginUserId
         })
 
-        if(insertId) {
+        if (insertId) {
             return this.success('数据保存成功')
         } else {
             return this.fail('数据保存失败')
