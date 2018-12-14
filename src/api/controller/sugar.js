dcb35a1a5fe3036943a2d26a0e702b4cb9916f75
Taylorzun <33285361+TaylorZun@users.noreply.github.com>
2018-12-14 18:08:02 +0800

    Add files via upload

--- src/api/controller/sugar.js
+++ src/api/controller/sugar.js
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: pxf
  * @Date: 2018-12-08 17:03:19
- * @LastEditTime: 2018-12-13 15:51:43
+ * @LastEditTime: 2018-12-14 17:25:18
  * @LastEditors: Please set LastEditors
  */
 
@@ -13,20 +13,20 @@ module.exports = class extends Base {
      * sssss
      */
     async sugarhandAction() {
-        let data = this.post()  ///获取到提交上来的所有数据
+        var data = this.post() ///获取到提交上来的所有数据
         console.log(data)
-        const sugardata = this.post(data.sugardata)
-        const timedian = this.post(data.timedian)
-        const beizhu = this.post(data.beizhu)
-        // const buffer = Buffer.from(beizhu)
-        const insertId = await this.model('sugardata').add({
-            sugardata: sugardata,
-            timedian: timedian,
-            // beizhu: buffer.toString('base64'),
-            beizhu: beizhu,
+        // const beizhu = data.beizhu
+        //  const buffer = Buffer.from(beizhu)
+        const sugar = this.model('sugardata')
+        const insertId = await sugar.add({
+            sugardata:data.sugardata,
+            timedian:data.timedian,
+            // beizhu: buffer.toString('base64'),哈哈哈这个地方暂时不需要了
+             beizhu: data.beizhu,
             add_time: this.getTime(),
             //user_id: this.getLoginUserId
         })
+        console.log(insertId)
 
         if (insertId) {
             return this.success('数据保存成功')
