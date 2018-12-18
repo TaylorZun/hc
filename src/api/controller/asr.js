f02c56877db5e4e7fdd379849a574e2732343481
Taylorzun <33285361+TaylorZun@users.noreply.github.com>
2018-12-18 17:40:29 +0800

    Add files via upload

--- src/api/controller/asr.js
+++ src/api/controller/asr.js
@@ -2,28 +2,65 @@
  * @Description: 语音识别 Automic speech recognition
  * @Author: pxf
  * @Date: 2018-12-17 16:42:55
- * @LastEditTime: 2018-12-17 17:58:59
+ * @LastEditTime: 2018-12-18 17:27:35
  * @LastEditors: Please set LastEditors
  * 
  */
 
- const Base = require ('./base.js')
- const fs = require('fs')
- const Multiparty = require('multiparty')
- const ffmpeg = require('fluent-ffmpeg')
- const AipSpeechServer = require('baidu-aip-sdk').speech
+const Base = require('./base.js')
+const fs = require('fs')
+const Multiparty = require('multiparty')
+const ffmpeg = require('fluent-ffmpeg')
+const AipSpeechServer = require('baidu-aip-sdk').speech
 
- const APP_ID = "15185039"
- const API_KEY = "ESn1t8mAiNZlGOfwPqvPUoFl"
- const SECRET_KEY = "uNFeWk2RT7ie4GnhmK18qNyRcSWZUsKL"
- 
+const APP_ID = "15185039"
+const API_KEY = "ESn1t8mAiNZlGOfwPqvPUoFl"
+const SECRET_KEY = "uNFeWk2RT7ie4GnhmK18qNyRcSWZUsKL"
 
- module.exports = class extends Base {
+
+module.exports = class extends Base {
 
     async asrAction() {
         //新建一个对象，建议只保存一个对象调用服务接口
         const client = new AipSpeechServer(APP_ID, API_KEY, SECRET_KEY)
         const voice = this.file()
+        const uploadedPath = voice.path
+        const command = ffmpeg()
+        command.addInput(uploadedPath)
+        .saveToFile('../../runtime/upload/audio/16k.wav')
+        .on('error', function(err){
+            console.log(err)
+        })
+        .on('end', function(){
+            const voices = fs.readFileSync('../../runtime/upload/audio/16k.wav')
+            let voiceBuffer = new Buffer(voices)
+            client.recognize(voiceBuffer, 'wav', 16000).then(function(result){
+                const data = []
+                if(result.err_no === 0){
+                    data = result.result
+                }
+            }, function(err){
+                console.log(err)
+            })
+
+            //删除上传的音频文件
+            fs.unlink(uploadedPath, function(err){
+                if(err) {
+                    console.log(uploadedPath + '文件删除失败')
+                }else {
+                    console.log('文件删除成功')
+                }
+            })
+            //删除转换格式的
+            fs.unlink('../../runtime/upload/audio/16k.wav', function(err){
+                if(err) {
+                    console.log('.wav delete failed')
+                }else {
+                    console.log('delete successfully')
+                }
+            })
+        })
+         
 
     }
- }
+}
