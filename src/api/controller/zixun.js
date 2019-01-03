eae063db12ea90c5194a192e895451b7a5d82579
Taylorzun <33285361+TaylorZun@users.noreply.github.com>
2019-01-03 11:26:49 +0800

    Add files via upload

--- src/api/controller/zixun.js
+++ src/api/controller/zixun.js
@@ -2,7 +2,7 @@
  * @Description: 关于医师咨询的相关的查询
  * @Author: your name
  * @Date: 2018-12-23 14:17:21
- * @LastEditTime: 2018-12-31 14:54:37
+ * @LastEditTime: 2019-01-03 10:17:47
  * @LastEditors: Please set LastEditors
  */
 
@@ -25,6 +25,15 @@ module.exports = class extends Base {
         return this.success({
             info: info
         })
+    }
+
+    async queryAction() {
+        const query = this.get('inputValue')
+        const model = this.model('doctor')
+        let doctorList = await model.where({'name|master|hospital' : ['like', '%'+query+'%']}).select()
+
+        return this.success(doctorList)
+        
     }
 
   
